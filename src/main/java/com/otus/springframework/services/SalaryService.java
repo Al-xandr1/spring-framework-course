5b978aa11cbdd06f460a36703fd79ce86007dceb
al-xandr1 <al-xandr1@yandex.ru>
2023-03-01 22:39:26 +0400

    Add additional logs into business methods

--- src/main/java/com/otus/springframework/services/SalaryService.java
+++ src/main/java/com/otus/springframework/services/SalaryService.java
@@ -1,11 +1,15 @@
 package com.otus.springframework.services;
 
 import com.otus.springframework.enities.Worker;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SalaryService {
+
+    private static Logger LOG = LoggerFactory.getLogger(SalaryService.class);
 
     @Value("${myapp.premiumTariff:0.2}")
     private double premiumTariff;
@@ -14,21 +18,29 @@ public class SalaryService {
     private double salaryTariff;
 
     public double calculateSalary(Worker worker) {
+        LOG.info("Method com.otus.springframework.services.SalaryService.calculateSalary was called with parameters {}", worker);
         var grade = worker.getGrade();
 
+        double sal;
         if (grade <= 5) {
-            return salaryTariff;
+            sal = salaryTariff;
 
         } else if (grade <= 10) {
-            return Math.round(salaryTariff * 1.5);
+            sal = salaryTariff * 1.5;
 
         } else {
-            return Math.round(salaryTariff * 2);
+            sal = salaryTariff * 2;
         }
+
+        LOG.info("Method com.otus.springframework.services.SalaryService.calculateSalary has returned result {}", sal);
+        return sal;
     }
 
     public double calculatePremium(Worker worker) {
+        LOG.info("Method com.otus.springframework.services.SalaryService.calculatePremium was called with parameters {}", worker);
         var salary = worker.getCurrentSalary();
-        return salary * premiumTariff;
+        double premium = salary * premiumTariff;
+        LOG.info("Method com.otus.springframework.services.SalaryService.calculatePremium has returned result {}", premium);
+        return premium;
     }
 }
